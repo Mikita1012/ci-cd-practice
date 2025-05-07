9da927d1c5fbf53758ee9e30d2a55a2622735355
Mikita.Shah <mikitashah2009@gmail.com>
2025-05-07 16:09:39 +0530

    Made changes in .yml file- added condition on deploy to happen on MAIN and CI runs on all branches. Added a footer component, to check if CI runs on feature branch and deploy happens only after PR with main branch.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import Counter from './Counter';
+import Footer from './Footer';
 import Hello from './Hello';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <> 
     <Hello />
     <Counter />
+    <Footer/>
     </>
       
   );
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='text-center mt-20'>
+        <p>© 2025 Mikita's CI/CD Practice 🚀</p>
+    </footer>
+  )
+}
+
+export default Footer
