a6cf4b724efc1d05541f644706a2fdd78dcda282
Mikita.Shah <mikitashah2009@gmail.com>
2025-05-07 17:05:45 +0530

    Added a comment to check for PR

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
+// FOOTER ELEMENT
 function Footer() {
   return (
     <footer className='text-center mt-20'>
         <p>© 2025 Mikita's CI/CD Practice 🚀</p>
-    </footer>
+    </footer> 
   )
 }
 
