f87579bbb2326ac869d0346fcf0426a123eb1333
Mikita.Shah <mikitashah2009@gmail.com>
2025-05-09 16:22:26 +0530

    A minor change: on buttons

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,9 +4,9 @@ function Counter() {
   const [count, setCount] = useState(0);
   return (
     <div className="d-flex flex-column align-items-center">
-      <button type="button" className="btn btn-outline-success btn-lg" onClick={() => setCount(count - 1)}>Subtract</button>
+      <button type="button" className="btn btn-outline-success btn-lg" onClick={() => setCount(count + 1)}>Add</button>
        <h1 className="m-2">{count}</h1>
-      <button className="btn btn-outline-success btn-lg" onClick={() => setCount(count + 1)}>Add</button>
+      <button className="btn btn-outline-success btn-lg" onClick={() => setCount(count - 1)}>Subtract</button>
     </div>
   );
 }
